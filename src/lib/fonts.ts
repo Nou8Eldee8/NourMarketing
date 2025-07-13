de953af6a171d74a198c7ab2fe1a6b5e36d2ac62
Nou8Eldee8 <131700301+Nou8Eldee8@users.noreply.github.com>
2025-07-13 15:35:03 +0300

    Fix font path for Netlify build

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -2,10 +2,11 @@ import localFont from "next/font/local";
 import { Dancing_Script } from "next/font/google";
 
 export const cocogoose = localFont({
-  src: "../public/fonts/Cocogoose-Pro-Regular-trial.ttf",
+  src: "../../public/fonts/Cocogoose-Pro-Regular-trial.ttf",
   variable: "--font-cocogoose",
   display: "swap",
 });
+
 
 export const dancingScript = Dancing_Script({
   subsets: ["latin"],
